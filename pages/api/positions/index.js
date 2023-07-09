1c78d0113a607726bef6af465e6eb65434f86d72
AlexisSalmeron <alexissalmeron.baez9@gmail.com>
2023-07-08 19:58:19 -0600

    La Tabla de Posiciones de Cada Liga funciona Correctamente

--- pages/api/positions/index.js
+++ pages/api/positions/index.js
@@ -1,74 +1,40 @@
-import db from "database/models";
+import db from "@/database/models";
+
 export default async function handler(req, res) {
   try {
-    // Obtener todos los registros de la tabla de posiciones
-    const posiciones = await db.PositionTableLeague.findAll();
+    const { leagueId } = req.query;
+    const positionTable = await db.PositionTableLeague.findAll({
+      where: { leagueId },
+      include: [
+        {
+          model: db.Club,
+          as: 'club',
+        },
+      ],
+      order: [['points', 'DESC']],
+    });
 
-    // Recorrer cada registro y actualizar los valores correspondientes
-    for (const posicion of posiciones) {
-      const { clubId } = posicion;
+    const results = positionTable.map((position) => ({
+      id: position.id,
+      matchesWon: position.matchesWon,
+      tiedMatches: position.tiedMatches,
+      //lostGames: position.lostGames,
+      //gamesPlayed: position.gamesPlayed,
+      gf: position.gf,
+      gc: position.gc,
+      dif: position.gf - position.gc,
+      points: position.points,
+      clubId: position.clubId,
+      leagueId: position.leagueId,
+      clubName: position.club.name,
+    }));
 
-      // Obtener los datos actualizados para el club correspondiente
-      const club = await db.Club.findByPk(clubId);
-
-      // Calcular los valores actualizados en función de los resultados de los partidos
-      const resultadosPartidos = await db.Match.findAll({
-        where: {
-          $or: [
-            { homeTeamId: clubId },
-            { visitorTeamId: clubId }
-          ]
-        }
-      });
-
-      let partidosGanados = 0;
-      let partidosEmpatados = 0;
-      let partidosPerdidos = 0;
-      let golesFavor = 0;
-      let golesContra = 0;
-
-      for (const resultado of resultadosPartidos) {
-        if (resultado.homeTeamId === clubId) {
-          golesFavor += resultado.scoreHome;
-          golesContra += resultado.scoreVisitor;
-
-          if (resultado.scoreHome > resultado.scoreVisitor) {
-            partidosGanados++;
-          } else if (resultado.scoreHome === resultado.scoreVisitor) {
-            partidosEmpatados++;
-          } else {
-            partidosPerdidos++;
-          }
-        } else {
-          golesFavor += resultado.scoreVisitor;
-          golesContra += resultado.scoreHome;
-
-          if (resultado.scoreVisitor > resultado.scoreHome) {
-            partidosGanados++;
-          } else if (resultado.scoreVisitor === resultado.scoreHome) {
-            partidosEmpatados++;
-          } else {
-            partidosPerdidos++;
-          }
-        }
-      }
-
-      // Actualizar los valores en la tabla de posiciones
-      await posicion.update({
-        matchesWon: partidosGanados,
-        tiedMatches: partidosEmpatados,
-        lostGames: partidosPerdidos,
-        gamesPlayed: partidosGanados + partidosEmpatados + partidosPerdidos,
-        gf: golesFavor,
-        gc: golesContra,
-        df: golesFavor - golesContra,
-        points: partidosGanados * 3 + partidosEmpatados,
-      });
-    }
-
-    res.status(200).json({ message: 'Tabla de posiciones actualizada' });
+    return res.json(results);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error al actualizar la tabla de posiciones' });
+    return res.status(500).json({
+      error: true,
+      message: 'Error al obtener los resultados de la tabla de posiciones',
+    });
   }
 }
